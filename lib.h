025e1d1ba0ce356611ee919688d0fac68f62789c
Carlos Eduardo <74271104+carlosedu757@users.noreply.github.com>
2022-07-07 10:02:19 -0300

    feat cria_turma

--- lib.h
+++ lib.h
@@ -1,14 +1,36 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<conio.h>
-struct UniaoBusca{
+/*struct UniaoBusca{
 	int n;
 	int *v; 
+};*/
+//FAZER STRUCT DE ALUNOS E OUTRO DE SALA DE AULA???
+
+struct aluno{
+	int num_chamada;
+	char *nome;
+	int *area_interesse;
 };
 
-UniaoBusca* ub_cria(int n);
-void ub_libera(UniaoBusca* ub);
-int ub_busca(UniaoBusca* ub, int x);
+struct gp_estudo{
+	int tam;
+	char *area;
+	aluno *participantes;
+};
+
+struct turma{
+	int tam_turma;
+	gp_estudo *grupos_estudo;
+	aluno *alunos;
+};
+
+/*UniaoBusca* ub_cria(int n);*/
+turma* cria_turma(int);
+/*void insere_alunos(*turma, int, char*, int*);*/
+/*void ub_libera(UniaoBusca* ub);*/
+int busca(aluno*, int);/*
 int ub_uniao(UniaoBusca* ub, int x, int y);
+void imprime(UniaoBusca* ub, int n);*/
 void menu();
-void imprime(UniaoBusca* ub, int n);
+
--- modulo.c
+++ modulo.c
@@ -1,26 +1,68 @@
 #include "lib.h"
 
-UniaoBusca* ub_cria (int n){
-
+/*UniaoBusca* ub_cria(int n){
 	int i;
-	UniaoBusca* ub = ( UniaoBusca*) malloc(sizeof ( UniaoBusca));
+	UniaoBusca* ub = (UniaoBusca*) malloc(sizeof(UniaoBusca));
 	ub->n = n;
-	ub->v = (int *) malloc(ub->n* sizeof (int ));	
+	ub->v = (int *) malloc(ub->n* sizeof(int));	
 	
-	for (i=0; i<ub->n; ++i)
+	for (i = 0; i < ub->n; ++i)
 		ub->v[i] = -1; 
 
 	return ub;
 
+}*/
+
+turma* cria_turma(int n){
+	int i;
+	turma* sala = (turma*) malloc (sizeof(turma));
+	sala->tam_turma = n;
+	sala->alunos = (aluno*) malloc (sala->tam_turma * sizeof(aluno));	
+	sala->grupos_estudo = (gp_estudo*) malloc (5 * sizeof(gp_estudo));
+	
+	sala->grupos_estudo[0]->area = "Estrutura de Dados";
+	sala->grupos_estudo[1]->area = "Fundamentos de Programacao";
+	sala->grupos_estudo[2]->area = "Banco de Dados";
+	sala->grupos_estudo[3]->area = "Calculo 1";
+	sala->grupos_estudo[4]->area = "Algebra Linear";
+	for(i = 0; i < 5; i++){
+		sala->grupos_estudo[i]->participantes = NULL;
+		sala->grupos_estudo[i].tam = 0;
+	}
+	
+	for (i = 0; i < alu->tam_turma; i++){
+		sala->alunos[i]->area_interesse = (int*) malloc (sizeof(int));
+		sala->alunos[i]->area_interesse[0] = -1;
+		sala->alunos[i]->nome = NULL;
+		sala->alunos[i]->num_chamada = 0;	
+	}
+
+	return sala;
 }
 
-void ub_libera ( UniaoBusca* ub){
+/*
+void ub_libera(UniaoBusca* ub){
 	free(ub->v);
 	free(ub);
-}
+}*/
 
-int ub_busca ( UniaoBusca* ub, int x){
+/*
+int ub_busca(UniaoBusca* ub, int x){
+	int r = x;
 	
+	while (ub->v[r] >= 0)
+		r = ub->v[r];
+	
+	while (ub->v[x] >= 0) {
+		int p = ub->v[x];
+		ub->v[x] = r;
+		x = p;
+	}
+	
+	return r;
+}*/
+
+int busca(aluno*, int){
 	int r = x;
 	
 	while (ub->v[r] >= 0)
@@ -35,7 +77,8 @@ int ub_busca ( UniaoBusca* ub, int x){
 	return r;
 }
 
-int ub_uniao ( UniaoBusca* ub, int x, int y){
+/*
+int ub_uniao(UniaoBusca* ub, int x, int y){
 
 	x = ub_busca(ub,x);
 	y = ub_busca(ub,y);
@@ -55,45 +98,56 @@ int ub_uniao ( UniaoBusca* ub, int x, int y){
 		ub->v[x] = y; 
 		return y;
 	}
-
 }
 
-void imprime (UniaoBusca* ub, int n){
+void imprime(UniaoBusca* ub, int n){
 	int i;
-	for (i=0; i<n;i++){
+	for (i = 0; i < n; i++){
 		printf("%d ", ub->v[i]);
 	}
-}
+}*/
 
 void menu(){
 	int op, elementoX, elementoY, tamanho;
-	UniaoBusca* conjunto;
+	turma* sala_aula;
 	//--------- iniciando a estrutura --------------------//
-    printf("\n -- UNIAO E BUSCA -- \n");
-    printf("Informe o numero de elementos do conjunto: ");
-    scanf("%d", &tamanho);
+    printf("\n -- SALA DE AULA -- \n");
+    printf("Informe o numero de alunos da sala de aula: ");
+    scanf(" %d", &tamanho);
     
-    conjunto = ub_cria(tamanho);
+    sala_aula = cria_turma(tamanho);
+    
+    for(i = 0; i < tamanho; i++){
+    	int disc = 0;
+    	sala_aula->alunos[i]->num_chamada = i + 1;
+    	printf("Digite o nome do aluno %d: ", sala_aula->alunos[i]->num_chamada);
+    	scanf(" %[^\n]", sala_aula->alunos[i]->nome);
+    	
+    	printf("\n -- ADICIONAR ALUNO A UM GRUPO DE ESTUDOS -- \n");
+    	printf("\n\t1 - Estrutura de Dados\n\t2 - Fundamentos de Programacao\n\t3 - Banco de Dados\n\t4 - Calculo 1\n\t5 - Algebra Linear\n");
+
+    	printf("Informe a disciplina: ");
+    	scanf(" %d", &disc);
+    			
+    	ub_uniao(conjunto, elementoX, elementoY);
+    	imprime(conjunto, tamanho);
+    	
+    	
+	}
+	
     do{    
     //--------------- Exibir menu ---------------------//
         system("cls");
-        puts("\n\t\t\t\tUNIAO E BUSCA\n");
-        puts("\t  \n \t1- Uniao  \n \t2 - Busca \n\t0  - SAIR");
+        puts("\n\t\t\t\tSALA DE AULA\n");
+        puts("\t\n\t1 - Adicionar um aluno a um grupo de estudos\n\t2 - Descobrir qual o grupo de estudos de um aluno\t\n\t3 - Listar todos os alunos\t\n\t4 - Lista dos grupos de estudos\t\n\t5 - Lista dos alunos por grupos de estudo\n\t0 - SAIR");
         printf("\nINFORME SUA OPCAO:\n");
         scanf("%d", &op);//Escolha da Opção
         
         switch(op)
         {
         //Opção de exibir lista
-    		case 1:{
-    			printf("\n -- UNIAO -- \n");
-    			printf("Informe um elemento: ");
-    			scanf("%d", &elementoX);
-    			printf("Informe um elemento: ");
-    			scanf("%d", &elementoY);
-    			
-    			ub_uniao(conjunto, elementoX, elementoY);
-    			imprime(conjunto, tamanho);
+        	case 0:{
+        		exit(0);
 				break;
 			}
     		
@@ -107,12 +161,26 @@ void menu(){
     			
 				break;
 			}
+			
+    		case 3:{
     			
+				break;
+			}
+			
+    		case 4:{
+    			printf("\n\t1 - Estrutura de Dados\n\t2 - Fundamentos de Programacao\n\t3 - Banco de Dados\n\t4 - Calculo 1\n\t5 - Algebra Linear\n");
+				break;
+			}
+			
+    		case 5:{
+    			
+				break;
+			}
     			
 	    	default:
 	    			puts("OPCAO INCORRETA,TENTAR NOVAMENTE.");break;
 	      
-	        }
+	    }
 		getch();
-	    }while(op!=0);
+	}while(op!=0);
 }
